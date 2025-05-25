0c72cd18f17f1cd7d34e3f0e701592a6c6eaa923
Amirjon Okilov <amirjonoqilov786@gmail.com>
2025-05-25 15:58:36 +0500

    First project about top payig jobs

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,18 @@
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM
+    job_postings_fact AS job
+LEFT JOIN company_dim AS company ON job.company_id = company.company_id
+WHERE
+    job_title_short = 'Data Scientist' AND
+    job_location ='Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY 
+    salary_year_avg DESC
+LIMIT 10;       
